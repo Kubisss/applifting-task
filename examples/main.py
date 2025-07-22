5d878b904c30b8d43d976a5684ef69d88d74791f
Kubisss <jakub.frydek@seznam.cz>
2025-07-22 15:20:29 +0200

    edited main file for local runs

--- examples/main.py
+++ examples/main.py
@@ -1,9 +1,33 @@
-import asyncio
+import asyncio, uuid
 from offers_sdk.client import OffersClient
+from offers_sdk.models import Product
+
 
 async def main():
-    client = OffersClient(refresh_token="YOUR_TOKEN", base_url="https://offers.api")
-    print(await client.ping())
+    client = OffersClient(
+        refresh_token="REFRESH_TOKEN_FROM_WEB",
+        base_url="https://python.exercise.applifting.cz/api/v1"
+    )
+
+    print("Is token valid? ", client.auth.is_token_valid())
+
+    # 1. Registering a product
+    try:
+        new_product = Product(id=uuid.uuid4(), name="iPhone", description="Latest Apple smartphone")
+        product = await client.register_product(new_product)
+        print(f"Product registered: {product}")
+    except Exception as e:
+        print(f"Registration failed: {e}")
+        return  # If the product registration failed, we stop with an error
+
+    # 2. Getting offers
+    try:
+        offers = await client.get_offers(str(product.id))
+        for offer in offers:
+            print(f"Offer {offer.id}: {offer.price} € (in stock {offer.items_in_stock})")
+    except Exception as e:
+        print(f"Loading offers failed: {e}")
+
 
 if __name__ == "__main__":
     asyncio.run(main())
