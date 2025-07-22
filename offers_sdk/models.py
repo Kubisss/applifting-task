94ae26e326ea309020f319117a569e8bfef7a1a3
Kubisss <jakub.frydek@seznam.cz>
2025-07-22 15:22:48 +0200

    id seriliazer and offer model

--- offers_sdk/models.py
+++ offers_sdk/models.py
@@ -1,7 +1,19 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, field_serializer, FieldSerializationInfo
+from typing import Any
+from typing import List
 from uuid import UUID
+
 
 class Product(BaseModel):
     id: UUID
     name: str
     description: str
+
+    @field_serializer("id")
+    def ser_id(self, value: Any, info: FieldSerializationInfo):
+        return str(value)
+    
+class Offer(BaseModel):
+    id: UUID
+    price: int
+    items_in_stock: int
