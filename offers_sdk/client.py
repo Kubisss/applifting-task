5bb4c04b465d1c6ab22d86305aae0ecea119741a
Kubisss <jakub.frydek@seznam.cz>
2025-07-27 20:23:07 +0200

    reformatted with Black and added exceptions

--- offers_sdk/client.py
+++ offers_sdk/client.py
@@ -1,24 +1,24 @@
+from urllib import response
 import httpx
 from typing import List
 from .models import Product, Offer
 from .auth import AuthManager
-
-
-class APIError(Exception):
-    # Exception during API calls.
-    pass
+from .exceptions import _raise_for_status
 
 
 class OffersClient:
     # Main client for communication with the Offers API.
-
     def __init__(self, refresh_token: str, base_url: str):
         self.base_url = base_url.rstrip("/")
         self.auth = AuthManager(refresh_token, base_url)
 
     async def _get_headers(self) -> dict:
         token = await self.auth.get_access_token()
-        return {"accept": "application/json", "Bearer": f"{token}", "Content-Type": "application/json"}
+        return {
+            "accept": "application/json",
+            "Bearer": f"{token}",
+            "Content-Type": "application/json",
+        }
 
     async def register_product(self, product: Product) -> Product:
         headers = await self._get_headers()
@@ -26,20 +26,16 @@ class OffersClient:
             response = await client.post(
                 f"{self.base_url}/products/register",
                 headers=headers,
-                json=product.model_dump()
+                json=product.model_dump(),
             )
-            try:
-                response.raise_for_status()
-            except httpx.HTTPStatusError as e:
-                raise Exception(f"Error while registering product: {response.text}") from e
+            _raise_for_status(response)
 
             data = response.json()
             return Product(
                 id=data.get("id", product.id),
                 name=product.name,
-                description=product.description
+                description=product.description,
             )
-
 
     async def get_offers(self, product_id: str) -> List[Offer]:
         # Returns a list of offers (GET /products/{product_id}/offers).
@@ -47,9 +43,7 @@ class OffersClient:
         headers = await self._get_headers()
         async with httpx.AsyncClient() as client:
             response = await client.get(
-                f"{self.base_url}/products/{product_id}/offers",
-                headers=headers
+                f"{self.base_url}/products/{product_id}/offers", headers=headers
             )
-        if response.status_code >= 400:
-            raise APIError(f"Error while fetching offers: {response.text}")
+        _raise_for_status(response)
         return [Offer(**offer) for offer in response.json()]
